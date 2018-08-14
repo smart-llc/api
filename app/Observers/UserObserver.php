b951d9cb982fb02db896f2b21ef12d5c79e3fb46
Gleb Karpushkin <rugleb@gmail.com>
2018-08-14 12:23:58 +0300

    created User observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+
+/**
+ * The User model observer.
+ *
+ * @author  Gleb Karpushkin  <rugleb@gmail.com>
+ *
+ * @package App\Observers
+ */
+class UserObserver
+{
+    //
+}
