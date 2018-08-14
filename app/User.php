4453bee304efc0cd62bb1adbae753dc7436c43a1
Gleb Karpushkin <rugleb@gmail.com>
2018-08-14 09:51:30 +0300

    remove password property

--- app/User.php
+++ app/User.php
@@ -2,12 +2,33 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * The User model.
+ *
+ * @property  int     $id
+ * @property  string  $name
+ * @property  string  $email
+ * @property  Carbon  $created_at
+ * @property  Carbon  $updated_at
+ *
+ * @author  Gleb Karpushkin  <rugleb@gmail.com>
+ *
+ * @package App
+ */
 class User extends Authenticatable
 {
     use Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +36,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email',
     ];
 
     /**
@@ -24,6 +45,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        //
     ];
 }
