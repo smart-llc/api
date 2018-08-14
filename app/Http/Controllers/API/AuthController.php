1b535acfdd9352788b95ff1a13762cf4a3c894d2
Gleb Karpushkin <rugleb@gmail.com>
2018-08-14 13:22:15 +0300

    created Auth controller

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * The API Auth controller.
+ *
+ * @author  Gleb Karpushkin  <rugleb@gmail.com>
+ *
+ * @package App\Http\Controllers\API
+ */
+class AuthController extends Controller
+{
+    //
+}
